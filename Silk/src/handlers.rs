bcce70e94002578bda8bffceb169fb230d1df42c
Alessandro Cresto Miseroglio <alessandro.cresto.miseroglio@gmail.com>
2024-09-15 19:37:56 +0200

    feat(silk): Implement subscribe.

--- Silk/src/handlers.rs
+++ Silk/src/handlers.rs
@@ -1,5 +1,7 @@
-use axum::{debug_handler, http::StatusCode, response::IntoResponse, extract::Form};
+use axum::{debug_handler, extract::{Form, State}, http::StatusCode, response::IntoResponse};
+use sqlx::SqlitePool;
 use tracing::debug;
+
 
 #[derive(serde::Deserialize, Debug)]
 pub struct Subscription {
@@ -7,7 +9,13 @@ pub struct Subscription {
 }
 
 #[debug_handler]
-pub async fn subscribe(Form(sub): Form<Subscription>) -> impl IntoResponse {
-    debug!("Subscribed with email: {}", sub.email);
-    StatusCode::OK
+pub async fn subscribe(State(state): State<SqlitePool>, Form(sub): Form<Subscription>) -> impl IntoResponse {
+    let query = format!("INSERT INTO subscriptions (email) VALUES ('{}')", sub.email);
+    if let Ok(_) = sqlx::query(&query).execute(&state).await {
+        debug!("Subscribed {}", sub.email);
+        StatusCode::OK
+    } else {
+        debug!("Failed to subscribe {}", sub.email);
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
 }
--- Silk/src/main.rs
+++ Silk/src/main.rs
@@ -4,10 +4,13 @@ use tower_http::trace::TraceLayer;
 use tracing::debug; // Use the Router and get types from the axum crate
 
 mod handlers;
+mod state;
 
 use handlers::subscribe; // Import the subscribe module
 
 const ADDR: &str = "127.0.0.1:4000"; // Address to bind to
+
+const DATABASE_URL: &str = "sqlite://db.sqlite"; // Database URL
 
 #[tokio::main] // Main function
 async fn main() -> Result<()> {
@@ -17,8 +20,13 @@ async fn main() -> Result<()> {
 
     tracing_subscriber::fmt::init(); // Initialize the logger
 
+    let pool = sqlx::sqlite::SqlitePool::connect(DATABASE_URL).await?; // Connect to the database
+
+
+
     let app = Router::new() // Create a new Router
         .route("/subscribe", post(subscribe)) // Add a route to the Router
+        .with_state(pool) // Add the AppContext to the Router
         .layer(TraceLayer::new_for_http()); // Add a tracing layer to the Router.
 
     let listener = tokio::net::TcpListener::bind(ADDR).await?; // Bind to the address
