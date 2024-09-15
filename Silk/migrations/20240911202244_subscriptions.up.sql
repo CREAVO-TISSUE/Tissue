52a9f3f842c73fb64ffe795be7e80ac0cc138d7c
Alessandro Cresto Miseroglio <alessandro.cresto.miseroglio@gmail.com>
2024-09-15 19:37:16 +0200

    Fix(silk): Upgrade migration.

--- Silk/migrations/20240911202244_subscriptions.up.sql
+++ Silk/migrations/20240911202244_subscriptions.up.sql
@@ -1,5 +1,6 @@
 -- Add up migration script here
-CREATE TABLE subscriptions (
+CREATE TABLE IF NOT EXISTS subscriptions
+(
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
