4bfa02cc1f7071c8c396b2276b9edb17963877c3
Alessandro Cresto Miseroglio <alessandro.cresto.miseroglio@gmail.com>
2024-09-16 12:56:37 +0200

    fix(silk): sqlite primary key.

--- Silk/migrations/20240911202244_subscriptions.up.sql
+++ Silk/migrations/20240911202244_subscriptions.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS subscriptions
 (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY ASC,
     email VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
