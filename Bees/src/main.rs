00e6eb4e410c0c0fb4aebc559245aa72d82b5d73
Alessandro Cresto Miseroglio <alessandro.cresto.miseroglio@gmail.com>
2024-10-02 10:32:09 +0200

    Add main handler.

--- Bees/src/main.rs
+++ Bees/src/main.rs
@@ -4,21 +4,23 @@ use axum::{
     Router,
 };
 use anyhow::Result;
+use tracing::info;
 
 // Create a axum handler with expose a Tokio server with a single route
 // The route should be a GET request to /fx/{amount}/{from}/
-async fn handler(
-) -> impl IntoResponse {
+async fn handler() -> impl IntoResponse {
     String::from("hello world")
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
     // Create a new router
     let app = Router::new().route("/fx", get(handler));
 
     // Start the server
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    info!("listening on {}", listener.local_addr()?);
     axum::serve(listener, app)
         .await?;
     Ok(())
